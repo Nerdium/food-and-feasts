b9cea1efac642040dd5ddf98f1a0691657842b5d
Nerdium <nerdiumlol@gmail.com>
2024-01-08 14:42:17 -0600

    initial commit

---
+++ src/client/java/nerdium/foodandfeasts/FoodAndFeastsClient.java
@@ -0,0 +1,10 @@
+package nerdium.foodandfeasts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FoodAndFeastsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
